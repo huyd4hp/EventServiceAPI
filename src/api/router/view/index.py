4fefadf8ef67a719b632b563cdbe52952bc3bfce
Nhật Huy <nhathuyd4hp@gmail.com>
2024-06-15 16:31:58 +0700

    deleted

--- src/api/router/view/index.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-from .endpoints.event import EventRouter
-from .endpoints.seat import SeatRouter
-ViewRouter = APIRouter()
-
-ViewRouter.include_router(EventRouter)
-ViewRouter.include_router(SeatRouter)
